a758e6c5a315e291e13c6069e96202415a8f71e1
linxiaobai <lin_music@126.com>
2016-01-15 17:02:24 +0800

    second commit

--- src/main/java/monitor/util/Constants.java
+++ src/main/java/monitor/util/Constants.java
@@ -4,11 +4,6 @@ package monitor.util;
  * Created by jianlin210349 on 2015/12/30.
  */
 public class Constants {
-    // 邮箱推送请求地址
-    public static final String SEND_EMAIL_URL = "http://admin.tv.sohuno.com/we/api/mail/send.do";
-
-    public static final String PUSH_EMAILS[] = {"hef@air-union.com", "jianlin210349@sohu-inc.com"};
-
     public static final String TJ_URL = "http://m.pm25.com/wap/city/tianjin.html";
     public static final String BJ_URL = "http://m.pm25.com/wap/city/beijing.html";
 
